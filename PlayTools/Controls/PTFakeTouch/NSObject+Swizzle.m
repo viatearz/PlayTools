dd19d1d2482ad6bb233e743e0e19b60da2de3fbe
viatearz <viatearz8056@outlook.com>
2025-08-04 20:32:37 +0800

    fix: reduce CPU usage by limiting motion update frequency

--- PlayTools/Controls/PTFakeTouch/NSObject+Swizzle.m
+++ PlayTools/Controls/PTFakeTouch/NSObject+Swizzle.m
@@ -14,6 +14,7 @@
 #import <VideoSubscriberAccount/VideoSubscriberAccount.h>
 #import <GameController/GameController.h>
 #import <AVFoundation/AVFoundation.h>
+#import <CoreMotion/CoreMotion.h>
 
 __attribute__((visibility("hidden")))
 @interface PTSwizzleLoader : NSObject
@@ -150,6 +151,15 @@ __attribute__((visibility("hidden")))
 
 - (void) hook_setCurrentSubscription:(VSSubscription *)currentSubscription {
     // do nothing
+}
+
+- (instancetype)hook_CMMotionManager_init {
+    CMMotionManager* motionManager = (CMMotionManager*)[self hook_CMMotionManager_init];
+    // The default update interval is 0, which may lead to high CPU usage
+    motionManager.accelerometerUpdateInterval = 0.01;
+    motionManager.deviceMotionUpdateInterval = 0.01;
+    motionManager.gyroUpdateInterval = 0.01;
+    return motionManager;
 }
 
 - (unsigned int)hook_applicationShouldTerminate:(id)sender {
@@ -315,6 +325,8 @@ bool menuWasCreated = false;
     // [objc_getClass("UITraitCollection") swizzleInstanceMethod:@selector(userInterfaceIdiom) withMethod:@selector(hook_userInterfaceIdiom)];
 
     [objc_getClass("VSSubscriptionRegistrationCenter") swizzleInstanceMethod:@selector(setCurrentSubscription:) withMethod:@selector(hook_setCurrentSubscription:)];
+
+    [objc_getClass("CMMotionManager") swizzleInstanceMethod:@selector(init) withMethod:@selector(hook_CMMotionManager_init)];
 
     if ([[PlaySettings shared] forceQuitOnClose]) {
         [objc_getClass("UINSApplicationDelegate") swizzleInstanceMethod:NSSelectorFromString(@"applicationShouldTerminate:") withMethod:@selector(hook_applicationShouldTerminate:)];
