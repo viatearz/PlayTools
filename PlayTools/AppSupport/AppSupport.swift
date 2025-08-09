34358ad15c4c1c9641882634dedf771e0204a5da
viatearz <viatearz8056@outlook.com>
2025-08-09 17:19:57 +0800

    fix: NIKKE hang and orientation issue

--- PlayTools/AppSupport/AppSupport.swift
+++ PlayTools/AppSupport/AppSupport.swift
@@ -22,3 +22,44 @@ extension AppSupport {
         return AppSupport()
     }()
 }
+
+extension AppSupport {
+    @objc func swizzleInstanceMethod(cls: AnyClass?, origSelector: Selector, newSelector: Selector) {
+        guard let originalMethod = class_getInstanceMethod(cls, origSelector),
+              let swizzledMethod = class_getInstanceMethod(cls, newSelector) else {
+            return
+        }
+
+        if class_addMethod(cls,
+                           origSelector,
+                           method_getImplementation(swizzledMethod),
+                           method_getTypeEncoding(swizzledMethod)) {
+            class_replaceMethod(cls,
+                                newSelector,
+                                method_getImplementation(originalMethod),
+                                method_getTypeEncoding(originalMethod))
+
+        } else {
+            if let imp = class_replaceMethod(cls,
+                                             origSelector,
+                                             method_getImplementation(swizzledMethod),
+                                             method_getTypeEncoding(swizzledMethod)) {
+                class_replaceMethod(cls,
+                                    newSelector,
+                                    imp,
+                                    method_getTypeEncoding(originalMethod))
+            }
+        }
+    }
+
+    @objc func swizzleClassMethod(cls: AnyClass?,
+                                  origSelector: Selector,
+                                  newSelector: Selector) {
+        guard let originalMethod = class_getClassMethod(cls, origSelector),
+              let swizzledMethod = class_getClassMethod(cls, newSelector) else {
+            return
+        }
+
+        method_exchangeImplementations(originalMethod, swizzledMethod)
+    }
+}
--- PlayTools/AppSupport/AppSupportRegistry.swift
+++ PlayTools/AppSupport/AppSupportRegistry.swift
@@ -4,5 +4,9 @@
 //
 
 class AppSupportRegistry {
-    static var lookup: [String: AppSupport.Type] = [:]
+    static var lookup: [String: AppSupport.Type] = [
+        "com.proximabeta.nikke": NIKKESupport.self,
+        "com.gamamobi.nikke": NIKKESupport.self,
+        "com.tencent.nikke": NIKKESupport.self
+    ]
 }
