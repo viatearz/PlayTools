1195bd47251630f991d128e4cf985b35efb62a56
viatearz <viatearz8056@outlook.com>
2025-10-23 19:14:11 +0800

    fix: RacingMaster(JP) stuck on Game Center Login

--- PlayTools/AppSupport/RacingMasterSupport.swift
+++ PlayTools/AppSupport/RacingMasterSupport.swift
@@ -8,6 +8,14 @@ class RacingMasterSupport: AppSupport {
         should_fix_available_memory = true
     }
 
+    override func applyHooks() {
+        // Fix login issue by skipping Game Center Login
+        self.swizzleInstanceMethod(
+            cls: NSClassFromString("GKLocalPlayer"),
+            origSelector: NSSelectorFromString("setAuthenticateHandler:"),
+            newSelector: #selector(NSObject.hook_RacingMaster_setAuthenticateHandler(_:)))
+    }
+
     override func postLaunch() {
         guard let bundleID = Bundle.main.bundleIdentifier else {
             return
@@ -22,5 +30,20 @@ class RacingMasterSupport: AppSupport {
 
         PlayCover.createSymbolicLink(source: URL(fileURLWithPath: srcPath),
                                      destination: URL(fileURLWithPath: destPath))
+    }
+}
+
+extension NSObject {
+    @objc func hook_RacingMaster_setAuthenticateHandler(_ handler: ((UIViewController?, Error?) -> Void)?) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(1)) {
+            // Simulate a "user canceled" result
+            if let handler = handler {
+                let userInfo = [
+                    NSLocalizedDescriptionKey: "The requested operation has been cancelled or disabled by the user."
+                ]
+                let error = NSError(domain: "GKErrorDomain", code: 2, userInfo: userInfo)
+                handler(nil, error)
+            }
+        }
     }
 }
